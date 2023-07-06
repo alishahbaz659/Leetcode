ebbef558c48405a9f95d9e473d34b01fe2ae7e25
AliShahbaz <alishahbazbal1234@gmail.com>
2023-07-06 12:14:23 +0500

    TwoSum problem bruteforce solution

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,18 +1,57 @@
-/**/
 
+/*
+*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*
+*/
 
+/*
+* Example 1:
 
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*
+*
+* */
+
+import java.sql.Array;
+import java.util.Arrays;
 
 public class TwoSum {
 
-    private void calculateTwoSum() {
-        System.out.println("Hello");
+    private int[] calculateTwoSum(int[] arr, int target) {
+        int[] indexes = new int[2];
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] + arr[j] == target) {
+                    indexes[0] = i;
+                    indexes[1] = j;
+                    return indexes;
+                    //   System.out.println(i + " " + j);
+                }
+            }
+        }
+        return indexes;
     }
-
 
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        twoSum.calculateTwoSum();
+        int[] arr = {3, 2, 4};
+        int target = 6;
+        int[] foundIndexes = twoSum.calculateTwoSum(arr, target);
+        System.out.println(Arrays.toString(foundIndexes));
 
     }
 }
