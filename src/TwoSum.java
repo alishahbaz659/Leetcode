b4cc9e9124b8bd85ca4f113eb502438378ec9d69
AliShahbaz <alishahbazbal1234@gmail.com>
2023-07-06 10:33:36 +0500

    First Commit

---
+++ src/TwoSum.java
@@ -0,0 +1,18 @@
+/**/
+
+
+
+
+public class TwoSum {
+
+    private void calculateTwoSum() {
+        System.out.println("Hello");
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        twoSum.calculateTwoSum();
+
+    }
+}
