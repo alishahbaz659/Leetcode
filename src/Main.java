938722193b355dda01dc3b9970f5a5e5b0398633
AliShahbaz <alishahbazbal1234@gmail.com>
2023-08-02 21:34:17 +0500

    Give a matrix containing 0 and 1 and as soon as there is one zero in a row it should replace every 0 in that row

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static String replaceSpace(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                stringBuilder.append("%");
+            } else {
+                stringBuilder.append(input.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        String userIn = input.nextLine();
+        System.out.println("Converted Input is: " + replaceSpace(userIn));
+
     }
 }
