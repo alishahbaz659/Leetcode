c78c1bdd07b3aae0c0b17832e74e56c22ac3d12f
AliShahbaz <alishahbazbal1234@gmail.com>
2023-08-02 15:28:31 +0500

    Count number of words in a sentence

---
+++ src/CountWordsInSentence.java
@@ -0,0 +1,26 @@
+public class CountWordsInSentence {
+
+
+    public static int wordsCounter(String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return 0;
+        }
+        System.out.println(sentence.length());
+
+        int count = 1; //start with 1 count
+        for (int i = 0; i < sentence.length() ; i++) {
+            if ((sentence.charAt(i) == ' ') && (sentence.charAt(i + 1) != ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        String inputString = "Ali Shahbaz";
+        System.out.println("Count of sentences are: " + wordsCounter(inputString));
+
+    }
+
+}
