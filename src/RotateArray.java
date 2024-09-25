2177b1db6cb1a23b374696d3c7dc37a1e877e56e
Ali <alishahbazbal1234@gmail.com>
2024-09-25 13:59:02 +0200

    Update RotateArray.java
    Update the solution of RotateArray problem

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -2,26 +2,33 @@ import java.util.Arrays;
 
 public class RotateArray {
 
-    public static void rotate(int[] nums, int k) {
-        int[] rotatedArray = new int[nums.length];
-        for (int r = 0; r < k; r++) {
-
-            for (int i = 0; i < nums.length; i++) {
-                if (i == 0) {
-                    rotatedArray[i] = nums[nums.length - 1];
-                } else {
-                    rotatedArray[i] = nums[i - 1];
-                }
-            }
-            //for leet code because it is checking original array for testing
-            System.arraycopy(rotatedArray, 0, nums, 0, nums.length);
-        }
-        System.out.println(Arrays.toString(nums));
+    public static int[] rotateArrayFun(int[] nums, int k){
+        
+        k = k%nums.length;      
+        RevereArray(nums,0,nums.length-1); 
+        RevereArray(nums,0,k-1);
+        RevereArray(nums, k, nums.length-1);
+ 
+        return nums;
     }
+    public static void RevereArray(int[] nums, int start, int end){
 
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
 
+            start++;
+            end--;
+        }
+    }
+    
+    
+    }
+    
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        rotate(nums, 3);
+        int[] nums = {1,2,3,4,5,6,7};
+        rotateArrayFun(nums, 3);
+        System.out.println("Reverse of the array is: "+ Arrays.toString(nums));
     }
 }
