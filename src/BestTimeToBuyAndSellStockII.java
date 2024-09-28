844348dc334b905c2135c7f8d4713557a0d7d636
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-09-28 16:38:25 +0200

    solve buy and sell stock II using valley peek approach

--- src/BestTimeToBuyAndSellStockII.java
+++ src/BestTimeToBuyAndSellStockII.java
@@ -1,20 +1,24 @@
 public class BestTimeToBuyAndSellStockII {
 
+
+    //O(n) valley peek approach
     public static int maxProfit(int[] prices) {
-        int maxProfit = 0;
+        int totalProfit = 0;
 
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i-1]) {
-                maxProfit += (prices[i] -prices[i-1]);
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += (prices[i] - prices[i - 1]);
             }
         }
 
-        return maxProfit;
+        return totalProfit;
     }
 
 
     public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4};
+        int[] prices = {7,1,5,3,6,4};
         System.out.println("Max total profit: " + maxProfit(prices));
+
+
     }
 }
