8e628424e05572f7fc8d82c852f93396681d7aff
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-09-29 23:46:57 +0200

    Solve Jump Game problem simplified with comment

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -1,3 +1,13 @@
+/*
+Jump Game
+
+You are given an integer array nums. You are initially positioned at the array's first index,
+and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+*/
+
 public class JumpGame {
 
     public static boolean canJump(int[] nums) {
