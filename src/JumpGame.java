7cf3f375a7619a84fbcf6646c8b931a60135bcc5
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-09-29 23:46:01 +0200

    Solve Jump Game problem simplified

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -6,11 +6,6 @@ public class JumpGame {
 
     public static void main(String[] args) {
         int[] nums = {2,3,1,1,4};
-
-        if(canJump(nums)){
-            System.out.println("You can reach the last index.");
-        }else {
-            System.out.println("You cannot reach the last index.");
-        }
+        System.out.println(canJump(nums));
     }
 }
