54322c5e9b9f8566f62d02977f92ef94e430a975
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-09-30 22:01:25 +0200

    Jump Game Solution

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -11,11 +11,19 @@ Return true if you can reach the last index, or false otherwise.
 public class JumpGame {
 
     public static boolean canJump(int[] nums) {
-        return true;
+
+        int finalPosition = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= finalPosition) {
+                finalPosition = i;
+            }
+        }
+
+        return (finalPosition == 0);
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,3,1,1,4};
+        int[] nums = {2, 0, 0};
         System.out.println(canJump(nums));
     }
 }
