9ebcc4186bd4773a3f3ee07487391c0249adc410
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-10-03 22:34:09 +0200

    solve HIndex leetcode problem

---
+++ src/HIndex.java
@@ -0,0 +1,11 @@
+public class HIndex {
+
+    public int hIndex(int[] citations){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        System.out.println();
+    }
+}
