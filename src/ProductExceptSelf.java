5df1ceba6a004a1fb3ea1a2eb34e313a052780b7
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-10-17 20:48:31 +0200

    product Except Self

---
+++ src/ProductExceptSelf.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+
+
+    public static int[] productExceptSelf(int[] nums) {
+        return new int[0];
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println("Product of array except self: " + Arrays.toString(productExceptSelf(nums)));
+    }
+}
