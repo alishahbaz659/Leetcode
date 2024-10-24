a43669cf615ed4007aa4ce280b45f7d1a1461b1d
Ali <79834087+alishahbaz659@users.noreply.github.com>
2024-10-24 21:21:18 +0200

    working

--- src/ProductExceptSelf.java
+++ src/ProductExceptSelf.java
@@ -14,6 +14,11 @@ public class ProductExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
         int length = nums.length;
         System.out.println("Length of the array is this:" + nums.length);
+        System.out.println("Length of the array is this:" + nums.length);
+        System.out.println("Length of the array is this:" + nums.length);
+        System.out.println("Length of the array is this:" + nums.length);
+        System.out.println("Length of the array is this:" + nums.length);
+        System.out.println("Length of the array is this:" + nums.length);
 
         return new int[0];
     }
